dc32f793c5fb0338c44290344cbe689505294a03
cafe3165 <78599297@qq.com>
2019-01-18 16:58:17 +0800

    have a test

--- src/main/java/com/cafe/spring_boot_hello/controller/HomeController.java
+++ src/main/java/com/cafe/spring_boot_hello/controller/HomeController.java
@@ -8,10 +8,9 @@ import com.cafe.spring_boot_hello.services.DevicesServices;
 import com.cafe.spring_boot_hello.services.LocationsServices;
 import com.cafe.spring_boot_hello.services.UsersServices;
 
-
 @RestController
 public class HomeController {
-	
+
 	@Autowired
 	private UsersServices user;
 	@Autowired
@@ -19,7 +18,6 @@ public class HomeController {
 	@Autowired
 	private List<DevicesServices> device;
 
-	
 	@RequestMapping("/user")
 	public String userName() {
 		user.setUserName("more");
@@ -29,31 +27,29 @@ public class HomeController {
 	}
 
 //	@SuppressWarnings("null")
-	@RequestMapping("userinroom")
+	@RequestMapping("/userinroom")
 	public String UserinLocation() {
 
 		user.setUserName("Jack");
 		location.setLocationName("Bedroom");
 		DevicesServices d1 = new DevicesServices();
 		DevicesServices d2 = new DevicesServices();
-		
+
 		d1.setDeviceName("Air-condition");
-		
-		System.out.println(d1.getDeviceName());
 		device.add(d1);
-		
 		d2.setDeviceName("Smart-Light");
-		System.out.println(d2.getDeviceName());
 		device.add(d2);
-		
 		System.out.println(device.get(1));
 		System.out.println(device.get(2));
 
-		String log1= "",log2="",log="";
-		log1= user.getUserName() + " in " + location.getLocationName()+" now ";
-		log2=device.get(1).getDeviceName()+" and "+	device.get(2).getDeviceName()+" in "+location.getLocationName();
-		log=log1+log2;
- 
+		String log1 = "", log2 = "", log = "";
+		log1 = user.getUserName() + " in " + location.getLocationName() + " now ";
+		log2 = device.get(1).getDeviceName() + " and " + device.get(2).getDeviceName() + " in "
+				+ location.getLocationName();
+		log = log1 + log2;
+		
+		System.out.println("666");
+
 		return log;
 
 	}
